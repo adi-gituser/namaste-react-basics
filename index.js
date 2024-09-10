4eb39a21c829316652e614e143f16b05e9fcb8a9
adi-gituser <aditya.kakkar.codes@gmail.com>
2024-09-10 20:41:42 +0530

    class last part
    index.html having react using crossorigin

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const h1 = document.createElement("h1");
-h1.innerHTML = "hello from actual js";
-const div = document.getElementById("main");
-div.appendChild(h1);
+const h1 = React.createElement("h1", { id: "heading" }, [
+  React.createElement(
+    "h2",
+    {
+      id: "headinsider",
+    },
+    "hello"
+  ),
+  React.createElement(
+    "h2",
+    {
+      id: "headinsider2",
+    },
+    "hello2"
+  ),
+  ,
+]);
+const div = ReactDOM.createRoot(document.getElementById("main"));
+div.render(h1);
+console.log(React);
+console.log(h1); // it is an object
